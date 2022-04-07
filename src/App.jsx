8c9ee422113e5afe0d48041a4eff1a5362cf15e7
rohit kumar <rohitsach50@gmail.com>
2022-04-07 23:22:42 +0530

    react-app using ViteJs

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Map from "./components/Map";
+import NavBar from "./components/NavBar";
+import Empty from "./Empty";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
+  //  console.log("render App");
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <div className="wrapper">
+          <Empty />
+          <NavBar />
+          <Map />
+        </div>
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'
+import App from './App';
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
